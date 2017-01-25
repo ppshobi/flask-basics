4158a15a8d8fffba863c587c400f5f78d64961ad
Shobi <ppshobi@gmail.com>
2017-01-25 11:46:40 +0530

    Render template

--- simple_app.py
+++ simple_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app=Flask(__name__)
 
@@ -12,6 +13,6 @@ def index(name="Shobi"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1,num2):
-	return "{} + {} = {}".format(num1,num2,num1+num2)
+	return render_template("add.html")
 
 app.run(debug=True, port=8000)
