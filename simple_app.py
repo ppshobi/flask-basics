f62f1d40be1456f246aa1b77d4862d0c05a3d979
Shobi <ppshobi@gmail.com>
2017-01-25 11:24:59 +0530

    Request object , extracting url

--- simple_app.py
+++ simple_app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
+from flask import request
 app=Flask(__name__)
 
 @app.route('/')
-def index():
-	return "Hello World"
+def index(name="Shobi"):
+	name=request.args.get('name',name)
+	return "Hello {}".format(name)
 
 app.run(debug=True, port=8000)
