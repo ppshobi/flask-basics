703354893f94e065b971bed36d7fa1a08ba8113e
Shobi <ppshobi@gmail.com>
2017-01-25 11:50:41 +0530

    passing values to template

--- simple_app.py
+++ simple_app.py
@@ -13,6 +13,7 @@ def index(name="Shobi"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1,num2):
-	return render_template("add.html")
+	context={'num1':num1,'num2':num2}
+	return render_template("add.html", **context)
 
 app.run(debug=True, port=8000)
